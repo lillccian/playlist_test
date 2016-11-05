951fd8b5f84e147f65d99e41e54ff697e08417c0
lillccian <lillccian@gmail.com>
2016-11-05 23:23:27 +0800

    add paginate

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
 	before_action :set_song, :only => [ :show, :edit, :update, :destroy ]
 	def index
-		@songs = Song.all
+		@songs = Song.page(params[:page]).per(10)
 	end
 
 	def new 
